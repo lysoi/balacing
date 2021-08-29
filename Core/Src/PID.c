89259c5665da8e78cf05dd464fcc0cdd6dfe17f8
lysoi <52250159+lysoi@users.noreply.github.com>
2021-08-29 08:50:19 +0700

    updata

---
+++ Core/Src/PID.c
@@ -0,0 +1,26 @@
+#include "PID.h"
+
+int PID(float setpoint, float input){
+	Kp = 1;
+	Kd = 0;
+	Ki = 0.00;
+    dt = 0.0116;
+	err = input - setpoint;
+
+	if(err > 0)
+		dir = -1;
+	else
+		dir = 1;
+	P = Kp*(err );
+	I = 0.5*Ki*dt*(err + err_1);
+	D = Kd/dt*(err - 2*err_1 + err_2);
+	PID_value = + ( (int) P +(int) I+(int)D);
+    err_1 = err;
+    err_2 = err_1;
+    PID_value_pre = PID_value;
+    return PID_value;
+//    if(PID_value>0)
+//    return PID_value + 570;
+//    else
+//    return PID_value - 570;
+}
